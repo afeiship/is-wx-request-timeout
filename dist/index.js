f28454fcbea2e2b28f167094dcb63e2d5558188b
afeiship <1290657123@qq.com>
2021-01-06 22:06:38 +0800

    Release 1.0.1

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,13 @@
  * name: @jswork/next-gm-storage
  * description: Storage implement for GM tampermonkey.
  * homepage: https://github.com/afeiship/next-gm-storage
- * version: 1.0.0
- * date: 2020-11-20 16:49:17
+ * version: 1.0.1
+ * date: 2021-01-06 22:06:33
  * license: MIT
  */
 
 (function() {
-  var global = global || this || window || Function('return this')();
+  var global = global || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var NxAbstractStorage = nx.AbstractStorage || require('@jswork/next-abstract-storage');
   var NxGmStoreEngine = nx.GmStoreEngine || require('@jswork/next-gm-store-engine');
