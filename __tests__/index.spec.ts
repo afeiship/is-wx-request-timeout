f91b1c410c883276f62817a9e5bcf184024945ad
afeiship <1290657123@qq.com>
2023-07-26 21:54:21 +0800

    feat: src done

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,19 @@
 import fn from '../src';
 
+const err1 = { errMsg: 'request:fail timeout' };
+const err2 = { errno: 5, errMsg: 'request:fail fail:time out' };
+const err3 = { errno: 600001, errMsg: 'error_msg:net::ERR_CONNECTION_RESET' };
+const err4 = { errMsg: 'request:fail -7:net::ERR_TIMED_OUT' };
+
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('when errmsg is undefined', () => {
+    expect(fn(undefined)).toBe(false);
+  });
+
+  test('Wx timeout normal case', () => {
+    expect(fn(err1.errMsg)).toBe(true);
+    expect(fn(err2.errMsg)).toBe(true);
+    expect(fn(err3.errMsg)).toBe(true);
+    expect(fn(err4.errMsg)).toBe(true);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-const IsWxRequestTimeout = (): void => {
-  console.log('hello');
+// { "errMsg": "request:fail timeout" }
+// { "errno": 5, "errMsg": "request:fail fail:time out" }
+// { "errno": 600001, "errMsg": "request:fail errcode:-101 cronet_error_code:-101 error_msg:net::ERR_CONNECTION_RESET" }
+// { "errMsg": "request:fail -7:net::ERR_TIMED_OUT" }
+
+declare var wx: any;
+
+const isWxRequestTimeout = (inErrorMessage): boolean => {
+  if (!inErrorMessage) return false;
+  const msg = inErrorMessage.toLowerCase();
+  if (msg.includes('timeout')) return true;
+  if (msg.includes('time out')) return true;
+  if (msg.includes('err_timed_out')) return true;
+  if (msg.includes('err_connection_reset')) return true;
+  return false;
 };
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-  module.exports = IsWxRequestTimeout;
+  module.exports = isWxRequestTimeout;
 }
 
-export default IsWxRequestTimeout;
+export default isWxRequestTimeout;
